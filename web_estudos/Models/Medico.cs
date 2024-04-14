8c70ac819ebed7030da940889dd28141d868e29c
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-04-14 13:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ web_estudos/Models/Medico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_estudos.Models
+{
+    public class Medico
+    {
+        public string CRM { get; set; }
+        public string Nome { get; set;}
+        public List<string> Especialidades { get; set;}
+
+        public Medico(string _crm)
+        {
+            this.CRM = _crm;
+            this.Especialidades = new List<string>();
+        }
+    }
+}
